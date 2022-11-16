098cb6778266c4248d57ed82da9eb2532eeb5f98
Logan <logan.tan@wsu.edu>
2022-11-16 14:56:39 -0800

    Added Hibernate Validator

--- src/main/java/com/revature/models/User.java
+++ src/main/java/com/revature/models/User.java
@@ -1,12 +1,12 @@
 package com.revature.models;
 
-import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @NoArgsConstructor
