1f489091ca4294c5a439cfd73fd67243220cd6b4
Justin Pan <105833041+jrprocks@users.noreply.github.com>
2022-12-02 10:43:59 -0500

    in progress

--- src/main/java/com/revature/dtos/PlaceOrderDTO.java
+++ src/main/java/com/revature/dtos/PlaceOrderDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import com.revature.models.Order;
 
 @Getter
 @Setter
@@ -14,4 +15,10 @@ public class PlaceOrderDTO {
     private int id;
     private int userId;
     private double subtotal;
+
+    public PlaceOrderDTO(Order order) {
+        this.setId(order.getId());
+        this.setUserId(order.getUser());
+        this.setSubtotal(order.getSubtotal());
+    }
 }
